05106c922229348781491e582c6d67518be3b793
CsMagicPekkaval <103623860+CsMagicPekkaval@users.noreply.github.com>
2025-10-14 22:09:27 +0800

    Update CppArray.cpp

--- CppArray.cpp
+++ CppArray.cpp
@@ -5,19 +5,28 @@ using namespace std;
 class CppArray {
 private:
     int size;
-    float* data;
+    float* value;
 
 public:
-    CppArray (int s = 8) {
+    CppArray (int s = 8,float num = 0) {
         size = s;
-        data = new float[size];
+        value = new float[size];
         for (int i = 0; i < size; i++)
-            data[i] = 0;
+            value[i] = num;
     }
 
     ~CppArray() {
-        delete[] data;
+        delete[] value;
     }
+
+    // 拷貝建構子
+    CppArray(const CppArray& other) {
+        size = other.size;
+        value = new float[size];
+        for (int i = 0; i < size; i++)
+            value[i] = other.value[i];
+    }
+
 
     // 取得大小
     int getSize() {
@@ -28,14 +37,15 @@ public:
     float& operator[](int index) {
         if (index < 0 || index >= size)
             throw out_of_range("out of range ERROR");
-        return data[index];
+        //cout<<value[index];
+        return value[index];
     }
 
     // cout
     friend ostream& operator<<(ostream& out, const CppArray& arr) {
         out << "{";
         for (int i = 0; i < arr.size; i++) {
-            out << arr.data[i];
+            out << arr.value[i];
             if (i != arr.size - 1)
                 out << ", ";
         }
@@ -46,28 +56,38 @@ public:
     // cin
     friend istream& operator>>(istream& in, CppArray& arr) {
         for (int i = 0; i < arr.size; i++) {
-            in >> arr.data[i];
+            in >> arr.value[i];
         }
         return in;
     }
-
+    //copy
     CppArray& operator=(const CppArray& other) {
-    delete[] data;
+    delete[] value;
     size = other.size;
-    data = new float[size];
+    value = new float[size];
 
     for (int i = 0; i < size; i++)
-        data[i] = other.data[i];
+        value[i] = other.value[i];
     return *this;
     }
 };
 
 int main() {
+    /*
     CppArray arr(3);
-    cin >> arr;            // 輸入
-    cout << arr << endl;   // 輸出
-    CppArray copyy(3);
+    cin >> arr;            
+    cout << arr << endl;
+    //cout << arr.getSize() << endl;
+    CppArray copyy(13);
     copyy = arr;
     cout << copyy << endl;;
+    cout << copyy[2];
+    CppArray a(3,4.5);
+    cout << a;
+*/
+    CppArray arr(3);
+    cin >> arr; 
+    CppArray a = arr;
+    cout << a;
     return 0;
 }
